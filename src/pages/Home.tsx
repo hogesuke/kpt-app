265370b3cf12782fec7feb8eab19d7bca2cf8b7e
hogesuke <miyado@gmail.com>
2025-11-30 23:06:29 +0900

    chore: Tailwindとshadcnをセットアップ

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { ReactElement } from 'react';
 
+import { Button } from "@/components/ui/button"
+
 export function Home(): ReactElement {
   return (
     <section className="space-y-4">
       <h1 className="text-2xl font-semibold">KPT APP</h1>
+      <Button>Button</Button>
     </section>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -7,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     tsconfigPaths()
   ],
   resolve: {
