fa3a680731561f578d8848f4fdc1d2e899651a14
hogesuke <miyado@gmail.com>
2026-01-28 23:35:07 +0900

    feat: ブロックスキップの実装

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ export function Layout(): ReactElement {
       <div className="flex h-screen flex-col">
         <Header />
 
-        <main className="min-h-0 flex-1 dark:bg-neutral-900">
+        <main id="main-content" className="min-h-0 flex-1 dark:bg-neutral-900">
           <ErrorBoundary key={location.pathname}>
             <Outlet />
           </ErrorBoundary>
